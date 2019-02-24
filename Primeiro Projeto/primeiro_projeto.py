65bda986c199fcb1da9bf99649c14fc96106e70a
Daniel Bailo <danbailoufms@gmail.com>
2019-02-23 20:22:12 -0400

    primeiro projeto - final

--- Primeiro Projeto/primeiro_projeto.py
+++ Primeiro Projeto/primeiro_projeto.py
@@ -1,23 +1,114 @@
+import random
+from IPython.display import clear_output
+def display_board(board):
+    clear_output()
+    for index, item in enumerate(board, start=0):
+        print(item, end=' | ' if index % 3 else '\n')
+
+def player_input():
+    market = ''
+
+    while not (market == 'X' or market == 'O'):
+        market = str(input('Player 1, você quer ser "X" ou "O"? ')).upper()
+
+        if market == 'X':
+            return ('X','O')
+        else:
+            return ('O','X')
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+def win_check(board,mark):
+    return( (board[1] == mark and board[2] == mark and board[3] == mark) or #primeira linha
+            (board[4] == mark and board[5] == mark and board[6] == mark) or #segunda linha
+            (board[7] == mark and board[8] == mark and board[9] == mark) or #terceira linha
+            (board[1] == mark and board[5] == mark and board[9] == mark) or #diagonal
+            (board[3] == mark and board[5] == mark and board[7] == mark) or #diagonal
+            (board[1] == mark and board[4] == mark and board[7] == mark) or #primeira coluna
+            (board[2] == mark and board[5] == mark and board[8] == mark) or #segunda coluna
+            (board[3] == mark and board[6] == mark and board[9] == mark)    #terceira coluna
+            )
+
+def choose_first():
+    if random.randint(0,1) == 0:
+        return('Player 1')
+    else:
+        return('Player 2')
+
+def space_check(board,position):
+    return board[position] == ' '
+
+def full_board_check(board):
+    for i in range(1,10):
+        if space_check(board,i):
+            return False
+        
+    return True
+
+def player_choice(board):
+    position = ' '
+
+    while position not in '1 2 3 4 5 6 7 8 9'.split() or not space_check(board, int(position)):
+        position = str(input('\nEscolha sua jogada (1-9): '))
+
+    return int(position)
+
+def replay():
+    return str(input('Quer jogar novamente? "Sim" ou "Não": ')).lower().startswith('s')
+
 def main():
-    i = 0
-    while(i!=4):
-        print('|-----------------------|')
-        print('|     Jogo da Velha     |')
-        print('|-----------------------|')
-        print('| (1) Iniciar Jogo      |')
-        print('| (2) Mostrar Tabuleiro |')
-        print('| (3) Reiniciar Jogo    |')
-        print('| (4) Sair Jogo         |')
-        i = int(input('Digite a opção desejada: '))
-        if i == 1:
-            pass
-        elif i == 2:
-            pass
-        elif i == 3:
-            pass
-        elif i == 4:
-            quit()
+    print('Bem vindo ao Jogo da Velha')
+
+    while True:
+        board = [' '] * 10
+        player1_marker,player2_marker = player_input()
+        turn = choose_first()
+        print(turn+' começa!')
+
+        game_on = True
+
+        while game_on:
+            #vez do jogador 1
+            if turn == 'Player 1':
+                display_board(board)
+                position = player_choice(board)
+                place_marker(board, player1_marker, position)
+
+            # checa vitória
+            if win_check(board, player1_marker):
+                display_board(board)
+                print('\nParabéns, você ganhou!\n')
+                game_on = False
+            else:
+                if full_board_check(board):
+                    display_board(board)
+                    print('\nEmpate!\n')
+                    break
+                else:
+                    turn = 'Player 2'
+
+            #vez do jogador 2
+            if turn == 'Player 2':
+                display_board(board)
+                position = player_choice(board)
+                place_marker(board, player2_marker, position)
+
+            # checa vitória
+            if win_check(board, player2_marker):
+                display_board(board)
+                print('\nParabéns, você ganhou!\n')
+                game_on = False
+            else:
+                if full_board_check(board):
+                    display_board(board)
+                    print('\nEmpate!\n')
+                    break
+                else:
+                    turn = 'Player 1'
+
+        if not replay():
+            break
 
 if __name__ == '__main__':
     main()
-
