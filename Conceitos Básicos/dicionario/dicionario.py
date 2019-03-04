c662f2f7dc2919d5f258cff968c2e0ddae92eeea
Daniel Bailo <danbailoufms@gmail.com>
2019-03-04 13:57:02 -0400

    att dicionario

--- Conceitos Básicos/dicionario/dicionario.py
+++ Conceitos Básicos/dicionario/dicionario.py
@@ -71,7 +71,7 @@ d = {'key1':1,'key2':2,'key3':3}
 print(d)
 
 # Retorna uma lista de todas as chaves
-print(d.keys())
+print('keys: ',d.keys())
 
 # Pega todos os valores
 print(d.values())
@@ -99,3 +99,26 @@ for v in d.values():
 
 for item in d.items():
     print(item)
+
+account_balance={}
+
+account_number = str(input())
+# deposit = float(input('Input a value of deposit: '))
+# account_balance[account_number] = deposit 
+
+for item in account_balance.items():
+    print(item)
+
+    print('Please, input a number of account: ')    
+    account_number = str(input())
+    # print(len(account_number))
+
+    while len(account_number) != 6 or account_number in account_balance:
+        if len(account_number) != 6:
+            print('Account must have 6 numbers: ')
+        elif account_number in account_balance:
+            print('This account has exist in bank, input a different number: ')
+        account_number = str(input())
+        
+        deposit = float(input('Input a value of deposit: '))
+        account_balance[account_number] = deposit 
