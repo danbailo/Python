786a776a4b15812fe1c43fc753888d12cdc2ed17
Daniel Bailo <danbailoufms@gmail.com>
2019-03-04 13:56:41 -0400

    exercicio usando dicionario e variavel global

--- Exercícios/banco/foo.py
+++ Exercícios/banco/foo.py
@@ -1,30 +1,34 @@
 start = True
 account = []
-deposit = []
+# deposit = []
 
 account_balance = {}
 
 def create_account():
-    global account,start,deposit,account_balance
+    global account,start,account_balance
 
     print('Please, input a number of account: ')    
     account_number = str(input())
     # print(len(account_number))
 
-    while len(account_number) != 6 or account_number in account:
+    while len(account_number) != 6 or account_number in account_balance:
         if len(account_number) != 6:
             print('Account must have 6 numbers: ')
-        elif account_number in account:
+        elif account_number in account_balance:
             print('This account has exist in bank, input a different number: ')
         account_number = str(input())
-
-
-    account.append(account_number)
+        
+    deposit = float(input('Input a value of deposit: '))
+    account_balance[account_number] = deposit 
+    # account.append(account_number)
 
 def show_accounts():
-    global account #, funciona sem
-    for i in enumerate(account):
-        print(i)
+    global account_balance
+    for item in account_balance.items():
+        print('Account {}, Balance: {}'.format(item[0],item[1]))
+    # global account #, funciona sem
+    # for i in enumerate(account):
+    #     print(i)
 
 def intro():
     global start
