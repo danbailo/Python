03e6bad2c6773674afe1a819412d47f52573fe06
Daniel Bailo <danbailoufms@gmail.com>
2019-03-06 15:42:10 -0400

    thread basic

---
+++ Thread/foo.py
@@ -0,0 +1,11 @@
+from threading import Thread
+import time
+
+def func(i):
+    print('Iniciando a thread %d' %i)
+    time.sleep(5)
+    print('Thread %d finalizada' %i)
+
+for i in range(10):
+    t = Thread(target=func, args=(i,))
+    t.start()
