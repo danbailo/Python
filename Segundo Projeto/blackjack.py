7bca58b2ce91ab118b260bf829dac7ce86aa4534
Daniel Bailo <danbailoufms@gmail.com>
2019-03-06 15:45:12 -0400

    att files

--- Segundo Projeto/blackjack.py
+++ Segundo Projeto/blackjack.py
@@ -1,3 +1,5 @@
+import random
+
 ranks = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'J', 'K']
 suits = ['♣', '♦', '♥', '♠']
 deck = []
@@ -13,6 +15,7 @@ class Card(object):
 
 class Deck(object):
     def __init__(self):
+        global deck
         self.deck = deck
         for rank in ranks:
             for suit in suits:
@@ -25,6 +28,42 @@ class Deck(object):
         deck_comp = "" #cria uma variavel string vazia
         for card in self.deck:
             deck_comp += " " + card.__str__()#card.__str__() isso aqui seria um A♣ por ex
-        return deck_comp    
+        return deck_comp
 
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+
+class Hand():
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+    
+    def __str__(self):
+        hand_comp = ""
+        
+        for card in self.cards:
+            card_name = card.__str__()
+            hand_comp += " " + card_name
+        
+        return "\nThe hand has {}".format(hand_comp)
+    
+    def card_add(self,card):
+        self.cards.append(card)
+
+        
+
+        
+
+#printando utilizando o objeto/desnecessario, ja q configuramos o str da class
+# Deck()
+# # print()
+# print(deck[0])
+
+Deck().shuffle() #embaralha o baralho
 print(Deck())
+
+# player = Hand()
+
+# player.card_add()
+print(Hand())
