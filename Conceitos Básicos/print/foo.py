5592564a6d788c86e66e8fa2c80d374b8a500034
Daniel Bailo <danbailoufms@gmail.com>
2019-03-06 16:50:26 -0400

    modificando o modo de printar

---
+++ Conceitos Básicos/print/foo.py
@@ -0,0 +1,18 @@
+#modificar a maneira de printar
+
+#padrao
+for i in range(10):
+    print(i)
+
+#padrao
+print('oi')
+print('tudo')
+print('bem?')
+
+for i in range(10):
+    print(i, end= ' ')
+
+print('\noi', end=' ')
+print('tudo', end=' ')
+print('bem', end='')#caracter vazio = nada
+print('?')
