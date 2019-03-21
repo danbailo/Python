9318365f55d80a468479aa93e3e196e2866a3ca0
Daniel Bailo <danbailoufms@gmail.com>
2019-03-21 14:03:29 -0400

    test dict

--- Conceitos Básicos/dicionario/foo_defaultdict.py
+++ Conceitos Básicos/dicionario/foo_defaultdict.py
@@ -4,14 +4,17 @@ from collections import defaultdict
 
 s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
 
-print(s)
+print('s is ',s)
+x = dict(s)
+print('x is ',x)
+
+
 d = defaultdict(list)
 print(d)
-
 for k,v in s:
     d[k].append(v)
 
-print(d.items())
+print(d)
 
-for k in d.values():
-    print(k)
+# for k in d.values():
+#     print(k)
