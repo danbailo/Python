5d3d63c68e37685f31ff1962757816dbfbe41f9d
Daniel Bailo <danbailoufms@gmail.com>
2019-03-23 18:50:02 -0400

    bozoh

--- Exercícios/bozoh/foo.py
+++ Exercícios/bozoh/foo.py
@@ -5,8 +5,8 @@ dice = [1, 2, 3, 4, 5, 6]
 class Player:
     def __init__(self, name):
         self.name = name
-        self.points = [['as','fu','sena'],['duque','seq','quarta'],['3',3,3],[4,4,4]]
-        # self.points = [[1,1,1],[2,2,2],[3,3,3],[4,4,4]]
+        # self.points = [['as','fu','sena'],['duque','seq','quarta'],['3',3,3],[4,4,4]]
+        self.points = [[1,1,1],[2,2,2],[3,3,3],[4,4,4]]
 
     def getname(self):
         return self.name
