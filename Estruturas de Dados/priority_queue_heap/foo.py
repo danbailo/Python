fc6f03d52ff68f797598c13a7507ebf577c28568
Daniel Bailo <danbailoufms@gmail.com>
2019-03-23 18:49:49 -0400

    priority queue heap

--- Estruturas de Dados/priority_queue_heap/foo.py
+++ Estruturas de Dados/priority_queue_heap/foo.py
@@ -14,31 +14,46 @@ class Pessoa(object):
 class FilaDePrioridade(object):
     
     def __init__(self):
-        self.__fila = []
-        self.__indice = 0
+        self.__fila = [] #esse seria o array
+        self.__indice = 0 #esse indice ordena os itens com o mesmo nivel de prioridade 
 
     def inserir(self, item, prioridade):
         heapq.heappush(self.__fila, (-prioridade, self.__indice, item)) #negando a prioridade, logo se torna uma max heap, ordenado de maior prioridade para menor prioridade
         self.__indice += 1
 
     def remover(self):
-        return heapq.heappop(self.__fila)[-1] #retorna o item(ultimo elemento da tupla) = [2]
+        return heapq.heappop(self.__fila)[-1] #esse indice só serve para retornar o "nome" da pessoa removida, visto que ele remove a tupla inteira, o ultimo elemento
+        # da tupla é o item, que seria o nome;
 
     def show(self):
         for i in self.__fila:
             print(i)
 
+    def print_array(self):
+        print(self.__fila)
+
+
+#A PRIORIDADE SERÁ A IDADE
+
+#As idades printarao negativo, pois na insercao eu neguei a mesma, porem, assim,
+#ela se tornou uma max heap(é o inverso da minheap, na minheap, os valores menores
+# viram primeiro, mas como é tudo negativo, o maior valor negativo, é na vdd o menor valor
+# entao ele vem a frente)
 
 fph = FilaDePrioridade()
 
-fph.inserir(Pessoa('Josué'),10)
-fph.inserir(Pessoa('Lucas'),15)
-fph.inserir(Pessoa('Daniel'),30)
-fph.inserir(Pessoa('Naul'),23)
-fph.inserir(Pessoa('Lucilei'),27)
+fph.inserir(Pessoa('Josué'),19)
+fph.inserir(Pessoa('Lucas'),22)
+fph.inserir(Pessoa('Daniel'),19)
+fph.inserir(Pessoa('Naul'),51)
+fph.inserir(Pessoa('Lucilei'),45)
 
-fph.show()
+# fph.show()
+fph.print_array()
 
 print(fph.remover())
 
-fph.show()
+fph.print_array()
+# fph.show()
+
+lst = [1,2,3]
