777236768b073c23e706738ee66e572ab6f153c6
Daniel Bailo <danbailoufms@gmail.com>
2019-03-26 15:12:25 -0400

    atribuindo valores a delta com funcao

--- Exercícios/afn_afd/foo.py
+++ Exercícios/afn_afd/foo.py
@@ -1,12 +1,3 @@
-from collections import defaultdict
-
-class Graph:
-	def __init__(self):
-		self.list_neighbor = defaultdict(list)
-
-	def add_edge(self, initial, final):
-		self.list_neighbor[initial].append(final)
-
 class AFN:
 	def __init__(self, states, sigma, delta, initial, final):
 		self.states = states
@@ -16,37 +7,41 @@ class AFN:
 		self.final = final
 
 	def get_states(self):
-		s = ['{}', self.states]
+		s = ['{}']
 		for j in self.states:
 			s.append((j))
 			for k in self.states:
 				if not j==k and (k,j) not in s:
-					s.append((j,k))
+					s.append((j,k))	
+		s.append(self.states)
 		return s
 
 	def get_sigma(self):
 		return self.sigma
 
 	def get_delta(self):
-		s = ['{}', self.states]
-		for j in self.states:
-			s.append((j))
-			for k in self.states:
-				if not j==k and (k,j) not in s:
-					s.append((j,k))
-		return s
+		pass
 
-delta = Graph()
+states = ('q1','q2','q3')
+sigma = ('a','b','ε')
+delta = []
+initial = 'q1'
+final = ('q1')
 
-delta.add_edge('q1','q2')
-delta.add_edge('q2','q2')
-delta.add_edge('q2','q3')
-delta.add_edge('q3','q1')
+def set_delta(states, sigma):
+	global delta
+	for i in states:
+		for j in sigma:
+			delta.append((i,j))
+	return delta
 
+set_delta(states,sigma)
 
-afd = AFN(('q1','q2','q3'), ('a','b'), 'delta', 'q1', ('q1'))
+print(delta)
 
-# print(afd.convert_states())
+afd = AFN(states, sigma, delta, initial, final)
+
 # print(afd.convert_states()[1][0])
+print(afd.get_states())
 # afd.get_states()
-afd.get_delta()
+# afd.get_delta()
