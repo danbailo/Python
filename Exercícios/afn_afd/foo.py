c9851ce07723745704c98e91708ba4dc3419c871
Daniel Bailo <danbailoufms@gmail.com>
2019-03-26 14:22:18 -0400

    convertendo afn para afd

---
+++ Exercícios/afn_afd/foo.py
@@ -0,0 +1,31 @@
+class AFN:
+	def __init__(self, states, sigma, delta, initial, final):
+		self.states = states
+		self.sigma = sigma
+		self.delta = delta
+		self.initial = initial
+		self.final = final
+
+	def get_states(self):
+		s = ['{}', self.states]
+		for j in self.states:
+			s.append((j))
+			for k in self.states:
+				if not j==k:
+					s.append((j,k))
+				if (k,j) in s:
+					s.remove((k,j))
+		return s
+
+	def get_sigma(self)
+		return self.sigma
+
+	def get_delta(self):
+		pass
+		
+afd = AFN(('q1','q2','q3'), 'ab', 'delta', 'q1', ['q1'])
+
+# print(afd.convert_states())
+# print(afd.convert_states()[1][0])
+# afd.convert_states2()
+print(afd.convert_states())
