479da7e9d752c23126bc35b161520fc895bb40f6
Daniel Bailo <danbailoufms@gmail.com>
2019-03-26 14:25:11 -0400

    get_states otimizado

--- Exercícios/afn_afd/foo.py
+++ Exercícios/afn_afd/foo.py
@@ -11,21 +11,19 @@ class AFN:
 		for j in self.states:
 			s.append((j))
 			for k in self.states:
-				if not j==k:
+				if not j==k and (k,j) not in s:
 					s.append((j,k))
-				if (k,j) in s:
-					s.remove((k,j))
 		return s
 
-	def get_sigma(self)
+	def get_sigma(self):
 		return self.sigma
 
 	def get_delta(self):
 		pass
-		
+
 afd = AFN(('q1','q2','q3'), 'ab', 'delta', 'q1', ['q1'])
 
 # print(afd.convert_states())
 # print(afd.convert_states()[1][0])
 # afd.convert_states2()
-print(afd.convert_states())
+print(afd.get_states())
