d8bd2bc15c9df74f03cd23a946c8b05e8e2d7a54
Daniel Bailo <danbailoufms@gmail.com>
2019-03-26 14:46:58 -0400

    testando o defaultdict para arestas

--- Exercícios/afn_afd/foo.py
+++ Exercícios/afn_afd/foo.py
@@ -1,3 +1,12 @@
+from collections import defaultdict
+
+class Graph:
+	def __init__(self):
+		self.list_neighbor = defaultdict(list)
+
+	def add_edge(self, initial, final):
+		self.list_neighbor[initial].append(final)
+
 class AFN:
 	def __init__(self, states, sigma, delta, initial, final):
 		self.states = states
@@ -19,11 +28,17 @@ class AFN:
 		return self.sigma
 
 	def get_delta(self):
-		pass
+		print(self.get_states())
 
-afd = AFN(('q1','q2','q3'), 'ab', 'delta', 'q1', ['q1'])
+# delta = {'q1':'q2,q3','q2':'q2,q3','q3':'q1'}
+
+delta = Graph()
+
+delta.add_edge('q1','q2')
+
+afd = AFN(('q1','q2','q3'), ('a','b'), 'delta', 'q1', ('q1'))
 
 # print(afd.convert_states())
 # print(afd.convert_states()[1][0])
-# afd.convert_states2()
-print(afd.get_states())
+# afd.get_states()
+afd.get_delta()
