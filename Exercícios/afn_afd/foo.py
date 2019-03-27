c51a713e1b139a71ee258c14a8458af75611ae53
Daniel Bailo <danbailoufms@gmail.com>
2019-03-27 18:06:09 -0400

    usando tuplas em tudo, vou mudar para listas

--- Exercícios/afn_afd/foo.py
+++ Exercícios/afn_afd/foo.py
@@ -22,25 +22,34 @@ class AFN:
 		return self.sigma
 
 	def get_delta(self):
-		print(self.delta)
+		print('delta: ',self.delta)
+		print('\nstates: ',self.get_states())
 
 states = ('q1','q2','q3')
 sigma = ('a','b','ε')
 
 #especificidade do automato
-delta = defaultdict(list)
-for i in states:
-	for j in sigma:
-		if j == 'a':
-			print('oi')
-		elif (i,j) == 'q2':
-			pass
-		elif (i,j) == 'q3':
-			pass
-			
-		delta[i,j].append('teste')
+# delta = [
+# 	['{}','q2','q3'],
+# 	[('q2','q3'),'q3','{}'],
+# 	['q1','{}','{}']
+# ]
 
-print(delta)
+# delta = defaultdict(list)
+# delta[('q1','a')].append('{}')
+
+delta = {}
+delta[('q1','a')] = ['{}']
+delta[('q1','b')] = ['q2']
+delta[('q1','ε')] = ['q3']
+delta[('q2','a')] = ['q2','q3']
+delta[('q2','b')] = ['q3']
+delta[('q2','ε')] = ['{}']
+delta[('q3','a')] = ['q1']
+delta[('q3','b')] = ['{}']
+delta[('q3','ε')] = ['{}']
+
+# print(delta)
 
 initial = 'q1'
 final = ('q1')
@@ -50,4 +59,4 @@ afd = AFN(states, sigma, delta, initial, final)
 # print(afd.convert_states()[1][0])
 # print(afd.get_states())
 # afd.get_states()
-# afd.get_delta()
+afd.get_delta()
