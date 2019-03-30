603b9c1cb992e63f48df6d8b082ba564b7a952c5
Daniel Bailo <danbailoufms@gmail.com>
2019-03-30 16:03:23 -0400

    indexacao de matriz, usando dicionario, deste modo, e possivel ver todos os valores de uma coluna

--- Exercícios/afn_afd/foo.py
+++ Exercícios/afn_afd/foo.py
@@ -19,89 +19,23 @@ class Convert_to_DFA:
 		return self.sigma
 
 	def get_delta(self):
-		for i in self.get_states():
-			print('states: ',i)
-		
-		for i in self.delta.keys():
-			print('keys: ',i)
-
-		for i in self.delta.items():
-			print('items: ',i)
-
-	def get_delta2(self):
-		new_delta = {}
-		n = 0
-		# print((self.states[0],self.states[1]))
-		# print((self.states[0],self.states[2]))
-		# print((self.states[1],self.states[0]))
-		# print((self.states[1],self.states[2]))
-		# print((self.states[0],self.states[1]))
-		# print(self.states[0]+self.states[1])
-		print('items')
-		for i in self.delta.items():
-			print(i)
-		print()
-		
-		for i in self.get_states():
-			print('states: ',i)
-			for j in self.get_sigma():
-				# print('delta values: ',self.delta)
-
-				# if i in self.delta.keys():
-				# 	print('im here')
-				if (i,j) in self.delta.keys():
-					print('here',i,j)
-					# new_delta[(i,j)] = self.delta[(i,j)]
-				elif i == self.states:
-					pass
-					# print('i: ',i)
-					# print('j: ',j)
-					# new_delta[(i,j)] = ['ok, I want repair state']
-							
-		return ''
+		for i in self.delta.values():
+			print(i[0])
+		pass
 
 
 states = ('q1','q2','q3')
-sigma = ('a','b') #i remove the 'ε'
+sigma = ('a','b') #I've remove the 'ε'
 
-#especificidade do automato
-# delta = [
-# 	['{}','q2','q3'],
-# 	[('q2','q3'),'q3','{}'],
-# 	['q1','{}','{}']
-# ]
-
-# delta = defaultdict(list)
-# delta[('q1','a')].append('{}')
-
+#a,b,ε
 delta = {}
-delta[('q1','a')] = ['{}']
-delta[('q1','b')] = ['q2']
-delta[('q1','ε')] = ['q3']
-delta[('q2','a')] = ['q2','q3']
-delta[('q2','b')] = ['q3']
-delta[('q2','ε')] = ['{}']
-delta[('q3','a')] = ['q1']
-delta[('q3','b')] = ['{}']
-delta[('q3','ε')] = ['{}']
-
-# print(delta)
-
-# print(delta)
+delta['q1'] = [['{}'], ['q2'], ['q3']]
+delta['q2'] = [['q2','q3'], ['q3'], ['{}']]
+delta['q3'] = [['q1'], ['{}'], ['{}']]
 
 initial = 'q1'
 final = ('q1')
 
 afd = Convert_to_DFA(states, sigma, delta, initial, final)
 
-# print(afd.convert_states()[1][0])
-# print(afd.get_states())
-# afd.get_states()
-
-# afd.get_delta()
-print(afd.get_delta2())
-
-
-
-# for i,k in afd.get_delta2().items():
-# 	print(i,k)
+afd.get_delta()
