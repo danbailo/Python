9add9490278af585520b9578da44c4788942c660
Daniel Bailo <danbailoufms@gmail.com>
2019-04-17 22:54:49 -0400

    biblioteca de desenhar

--- Snippets/draw_turtle/main.py
+++ Snippets/draw_turtle/main.py
@@ -43,7 +43,7 @@ for i in range(2):
 		myPen.speed(10)
 		myPen.pensize(5)
 		fontColor = "red"
-		message = 'vem de boquinha seu merda UUUUHUHUHhhhhhhhhhhhhhhhhh...............'
+		message = 'chupa meu pau seu bosta kkkkkkk...............'
 
 	displayMessage(message,fontSize,fontColor,-600,0)
 	myPen.clear()
