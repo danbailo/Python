84be83b3c75c7456ed48780eb66cf7d7013ca4dd
Daniel Bailo <danbailoufms@gmail.com>
2019-04-19 19:41:14 -0400

    pesquisar no google

---
+++ Snippets/google_search/foo.py
@@ -0,0 +1,17 @@
+from googlesearch import search
+
+query = "GitHub"
+
+for result in search(query, stop=10):
+    print(result)
+
+# https://github.com/
+# https://github.com/github
+# https://github.com/enterprise
+# https://github.com/join
+# https://desktop.github.com/
+# https://github.com/explore
+# https://en.wikipedia.org/wiki/GitHub
+# https://github.blog/
+# https://github.blog/2019-01-07-new-year-new-github/
+# https://twitter.com/github?lang=en
