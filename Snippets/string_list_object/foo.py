562fbce23570be224921621c18a1867cc517b934
Daniel Bailo <danbailoufms@gmail.com>
2019-04-19 19:43:25 -0400

    explicaco de ponteiro de string e lista/objeto

---
+++ Snippets/string_list_object/foo.py
@@ -0,0 +1,15 @@
+a = "Daniel"
+b = "Daniel"
+print(a == b)
+print(a is b)
+#strings sao imutaveis, quando iguais tem o mesmo conteudo
+#e apontam para o mesmo endereco de memoria
+
+c = [1,2,3]
+d = [1,2,3]
+print(c == d)
+print(c is d)
+#listas sao mutaveis, logo, quando iguais, tem o mesmo
+#conteudo, porem apontam para enderecos diferentes
+
+#quando apontam para o mesmo endereco, se diz o mesmo objeto
