b43f5e233b949b411570ef37d598a395bc4eaada
Daniel Bailo <danbailoufms@gmail.com>
2019-05-05 19:34:17 -0400

    passando argumentos na linha de comando

---
+++ Conceitos Básicos/argumentos_linha_comando/foo.py
@@ -0,0 +1,22 @@
+import sys
+
+def soma(n1, n2):
+    return n1+n2
+
+def sub(n1, n2):
+    return n1-n2
+
+def mult(n1, n2):
+    return n1*n2
+
+def div(n1, n2):
+    return n1/n2
+
+if sys.argv[1] == "soma":
+    print(soma(int(sys.argv[2]), int(sys.argv[3])))
+if sys.argv[1] == "sub":
+    print(sub(int(sys.argv[2]), int(sys.argv[3])))
+if sys.argv[1] == "mult":
+    print(mult(int(sys.argv[2]), int(sys.argv[3])))
+if sys.argv[1] == "div":
+    print(div(int(sys.argv[2]), int(sys.argv[3])))
