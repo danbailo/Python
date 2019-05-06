a2c47ed5975ea120e30145559999a7291d8ce33b
Daniel Bailo <danbailoufms@gmail.com>
2019-05-06 01:22:31 -0400

    ipython biblioteca

---
+++ Snippets/IPython/test.py
@@ -0,0 +1,9 @@
+# Comece com algumas importações!
+
+from __future__ import print_function
+from ipywidgets import interact, interactive, fixed
+import ipywidgets as widgets
+def f(x):
+	return x
+
+interact(f, x=10,)
