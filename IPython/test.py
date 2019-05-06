2fae48d67f709a45a88de8415692843eff3fed72
Daniel Bailo <danbailoufms@gmail.com>
2019-05-06 01:24:47 -0400

    rmv ipython

--- IPython/test.py
+++
@@ -1,9 +0,0 @@
-# Comece com algumas importações!
-
-from __future__ import print_function
-from ipywidgets import interact, interactive, fixed
-import ipywidgets as widgets
-def f(x):
-	return x
-
-interact(f, x=10,)
