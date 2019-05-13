860935351b5ad8b2c47bf92a95b22c023f0c4f61
Daniel Bailo <danbailoufms@gmail.com>
2019-05-12 20:12:43 -0400

    manipulando json

--- JSON/handling_json.py
+++ JSON/handling_json.py
@@ -1,1 +1,9 @@
 #https://www.datacamp.com/community/tutorials/json-data-python
+#https://www.w3schools.com/python/python_json.asp
+
+import pandas as pd
+
+data = pd.read_json("https://api.github.com/users")
+df = pd.DataFrame(data)
+
+print(df)
